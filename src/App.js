d0b1a74b78f099ff1c1b54dcccc96ac4ae12023c
vsp <vishnusureshperumbavoor@gmail.com>
2022-05-09 11:29:27 +0530

    msp is not a function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
-
 function App() {
+  const [toDos,setTodos] = useState('')
+  const [toDo,setTodo] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onClick={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,toDo])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        { toDos.map((value)=>{          
+            return (
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>{value}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>        
+            )
+        })
+        }
+      </div>
     </div>
   );
 }
